a52fad466f1c8ee7cc53717ffae7bffa13b8279e
kngenproject <niamniam701@gmail.com>
2026-06-29 18:49:01 +0700

    update

--- sw.js
+++ sw.js
@@ -1,14 +1,10 @@
 self.addEventListener('install', () => self.skipWaiting());
-
 self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(keys.map(k => caches.delete(k)))
-        )
-    );
+    event.waitUntil(caches.keys().then(keys =>
+        Promise.all(keys.map(k => caches.delete(k)))
+    ));
     self.clients.claim();
 });
-
 self.addEventListener('fetch', event => {
     event.respondWith(
         fetch(event.request, { cache: 'no-store' })
